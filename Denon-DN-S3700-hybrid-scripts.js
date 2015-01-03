bb51a60b02fd03c54166a2f701acc1542d4050d4
HoneyHazard <zeromemory@gmail.com>
2015-01-02 22:17:56 -0800

    printString function

--- Denon-DN-S3700-hybrid-scripts.js
+++ Denon-DN-S3700-hybrid-scripts.js
@@ -1,57 +1,32 @@
 function DenonDNS3700() {}
 
-DenonDNS3700.initDisplayCounter = 8;
+DenonDNS3700.m_initDisplayCounter = 8;
 
-DenonDNS3700.LedEnum = {
+DenonDNS3700.LedMode = {
     On: 0x4A,
     Off: 0x4B,
     Blink: 0x4C
 }
 
-DenonDNS3700.cmdCode = 0xB0;
+DenonDNS3700.c_cmdCode = 0xB0;
+
+DenonDNS3700.c_maxNumChars = 24;
+
+DenonDNS3700.c_emptyChar = " ".charCodeAt(0);
 
 DenonDNS3700.init = function (id)
 {
-    // blink TAP button
-    this.tapLed(this.LedEnum.On);
+    this.tapLed(this.LedMode.On);
 
-    this.printChar(0, "0".charCodeAt(0));
-    this.printChar(1, "1".charCodeAt(0));
-    this.printChar(2, "2".charCodeAt(0));
-    this.printChar(3, "3".charCodeAt(0));
+    this.printString("Hello, Mixxx");
 
-    this.printChar(4, "4".charCodeAt(0));
-    this.printChar(5, "5".charCodeAt(0));
-    this.printChar(6, "6".charCodeAt(0));
-    this.printChar(7, "7".charCodeAt(0));
-
-    this.printChar(8, "8".charCodeAt(0));
-    this.printChar(9, "9".charCodeAt(0));
-    this.printChar(10, "A".charCodeAt(0));
-    this.printChar(11, "B".charCodeAt(0));
-
-    this.printChar(12, "C".charCodeAt(0));
-    this.printChar(13, "D".charCodeAt(0));
-    this.printChar(14, "E".charCodeAt(0));
-    this.printChar(15, "F".charCodeAt(0));
-
-    this.printChar(16, "0".charCodeAt(0));
-    this.printChar(17, "1".charCodeAt(0));
-    this.printChar(18, "2".charCodeAt(0));
-    this.printChar(19, "3".charCodeAt(0));
-    
-    this.printChar(20, "4".charCodeAt(0));
-    this.printChar(21, "5".charCodeAt(0));
-    this.printChar(22, "6".charCodeAt(0));
-    this.printChar(23, "7".charCodeAt(0));
-
-    AmericanAudioDV2.initFlashTimerID
-        = engine.beginTimer(500, "DenonDNS3700.initFlashTimerHandler");
+    this.m_initFlashTimerID
+        = engine.beginTimer(500, "DenonDNS3700.initDisplayTimerHandler");
 }
 
 DenonDNS3700.tapLed = function(mode)
 {
-    midi.sendShortMsg(this.cmdCode, mode, 0x09);    
+    midi.sendShortMsg(this.c_cmdCode, mode, 0x09);    
 }
 
 DenonDNS3700.printChar = function(idx, ch)
@@ -62,14 +37,28 @@ DenonDNS3700.printChar = function(idx, ch)
     var idxMsb = 0x01 + idx;
     var idxLsb = 0x21 + idx;
     
-    midi.sendShortMsg(this.cmdCode, idxMsb, (ch & 0xF0) >> 4);
-    midi.sendShortMsg(this.cmdCode, idxLsb, ch & 0x0F);
+    midi.sendShortMsg(this.c_cmdCode, idxMsb, (ch & 0xF0) >> 4);
+    midi.sendShortMsg(this.c_cmdCode, idxLsb, ch & 0x0F);
 }
 
-DenonDNS3700.initFlashTimerHandler = function()
+DenonDNS3700.printString = function(str)
 {
-    if (DenonDNS3700.initFlashCounter % 2 == 0) {
-        // TODO something
+    for (i = 0; i < this.c_maxNumChars; ++i) {
+        if (i < str.length) {
+            this.printChar(i, str.charCodeAt(i));
+        } else {
+            this.printChar(i, this.c_emptyChar);
+        }
     }
+}
+
+DenonDNS3700.initDisplayTimerHandler = function()
+{
+    this.tapLed(this.LedMode.Blink);
+    
+    if (this.m_initDisplayCounter == 0) {
+        engine.stopTimer(this.m_initDisplayCounter);
+    }
+    --this.m_initDisplayCounter;    
 }
 
