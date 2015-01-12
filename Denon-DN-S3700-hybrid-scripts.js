8386eaa4969a30f69b73405d761c935d13407b64
HoneyHazard <zeromemory@gmail.com>
2015-01-12 15:55:40 -0800

    Properly impremented putChar

--- Denon-DN-S3700-hybrid-scripts.js
+++ Denon-DN-S3700-hybrid-scripts.js
@@ -29,8 +29,24 @@ DenonDNS3700.DisplayState = {
 // TODO: Display States
 
 DenonDNS3700.CMD_CODE = 0xB0;
-DenonDNS3700.MAX_NUM_CHARS = 24;
+DenonDNS3700.MAX_NUM_CHARS = 16;
 DenonDNS3700.EMPTY_CHAR = " ".charCodeAt(0);
+
+// Just hard code the midi numbers rathen than try to figure out the bizarre allocation
+// scheme by the manufacturer
+DenonDNS3700.CHAR_MSBS = [
+    [ 0x01, 0x02, 0x03, 0x04, 0x05, 0x07, 0x08, 0x09,
+      0x0A, 0x0B, 0x0C, 0x0D, 0x58, 0x59, 0x5A, 0x5B ],
+    [ 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+      0x16, 0x17, 0x18, 0x19, 0x6C, 0x6D, 0x6E, 0x6F ]
+];
+
+DenonDNS3700.CHAR_LSBS = [
+    [ 0x21, 0x22, 0x23, 0x24, 0x25, 0x27, 0x28, 0x29,
+      0x2A, 0x2B, 0x2C, 0x2D, 0x5C, 0x5D, 0x5E, 0x5F ],
+    [ 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
+      0x36, 0x37, 0x38, 0x39, 0x70, 0x75, 0x76, 0x77 ]
+];
 
 DenonDNS3700.initDisplayCounter = 8;
 DenonDNS3700.playbackState = DenonDNS3700.PlaybackState.Initializing;
@@ -40,6 +56,12 @@ DenonDNS3700.init = function (id)
 {
     // Does not work in hybrid mode :(
     // Is there a way to start up in a known platter state?
+
+    //DenonDNS3700.printLine1("12345678901234567890");
+    //DenonDNS3700.printLine1("test");
+    //DenonDNS3700.tapLed(DenonDNS3700.LedMode.On);
+    //DenonDNS3700.putChar(1, 3, "_".charCodeAt(0));
+
     DenonDNS3700.turntableOff();
     
     DenonDNS3700.tapLed(DenonDNS3700.LedMode.Blink);
@@ -93,13 +115,10 @@ DenonDNS3700.parametersLed = function(mode)
     midi.sendShortMsg(DenonDNS3700.CMD_CODE, mode, 0x1E);
 }
 
-DenonDNS3700.printChar = function(idx, ch)
+DenonDNS3700.putChar = function(row, col, ch)
 {
-    if (idx >= 5) {
-        idx++; // quirky offsets (see product documentation)
-    }
-    var idxMsb = 0x01 + idx;
-    var idxLsb = 0x21 + idx;
+    var idxMsb = DenonDNS3700.CHAR_MSBS[row][col];
+    var idxLsb = DenonDNS3700.CHAR_LSBS[row][col];
     
     midi.sendShortMsg(DenonDNS3700.CMD_CODE, idxMsb, (ch & 0xF0) >> 4);
     midi.sendShortMsg(DenonDNS3700.CMD_CODE, idxLsb, ch & 0x0F);
@@ -107,37 +126,22 @@ DenonDNS3700.printChar = function(idx, ch)
 
 DenonDNS3700.printLine1 = function(str)
 {
-    var half = DenonDNS3700.MAX_NUM_CHARS / 2;
-    for (i = 0; i < half; ++i) {
+    for (i = 0; i < DenonDNS3700.MAX_NUM_CHARS; ++i) {
         if (i < str.length) {
-            DenonDNS3700.printChar(i, str.charCodeAt(i));
+            DenonDNS3700.putChar(0, i, str.charCodeAt(i));
         } else {
-            DenonDNS3700.printChar(i, DenonDNS3700.EMPTY_CHAR);
+            DenonDNS3700.putChar(0, i, DenonDNS3700.EMPTY_CHAR);
         }
     }
 }
 
 DenonDNS3700.printLine2 = function(str)
 {
-    var half = DenonDNS3700.MAX_NUM_CHARS / 2;
-    for (i = 0; i < half; ++i) {
-        var iAdjusted = i + half;
-        if (i < str.length) {
-            DenonDNS3700.printChar(iAdjusted, str.charCodeAt(i));
-        } else {
-            DenonDNS3700.printChar(iAdjusted, DenonDNS3700.EMPTY_CHAR);
-        }
-    }
-}
-
-
-DenonDNS3700.printString = function(str)
-{
     for (i = 0; i < DenonDNS3700.MAX_NUM_CHARS; ++i) {
         if (i < str.length) {
-            DenonDNS3700.printChar(i, str.charCodeAt(i));
+            DenonDNS3700.putChar(1, i, str.charCodeAt(i));
         } else {
-            DenonDNS3700.printChar(i, DenonDNS3700.EMPTY_CHAR);
+            DenonDNS3700.putChar(1, i, DenonDNS3700.EMPTY_CHAR);
         }
     }
 }
@@ -149,10 +153,12 @@ DenonDNS3700.initDisplayTimerHandler = function()
         DenonDNS3700.printLine2("\\  Mixxx   /");
         DenonDNS3700.tapLed(DenonDNS3700.LedMode.On);
     } else if (DenonDNS3700.initDisplayCounter % 2 == 0) {
-        DenonDNS3700.printString("012345789ABCDEFGHIJKLMNO");
+        DenonDNS3700.printLine1("12345678901234");
+        DenonDNS3700.printLine2("ABCDEFGHIJKLMN");
         DenonDNS3700.tapLed(DenonDNS3700.LedMode.On);
     } else {
-        DenonDNS3700.printString("");
+        DenonDNS3700.printLine1("");
+        DenonDNS3700.printLine2("");
         DenonDNS3700.tapLed(DenonDNS3700.LedMode.Off);
     }
     
